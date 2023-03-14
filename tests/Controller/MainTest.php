7b7d99e7b9a93625ede138a9073b28f1f74914a0
Caliendo Julien <caliendo@hotmail.fr>
2023-03-14 12:00:08 +0100

    :sparkles: Création de la page d'accueil

---
+++ tests/Controller/MainTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MainTest extends WebTestCase
+{
+    public function testPageHome(): void
+    {
+        $client = static::createClient();
+        // Je me rend sur l'url "/"
+        $crawler = $client->request('GET', '/');
+        // Je vérifie que l'url existe
+        $this->assertResponseIsSuccessful();
+        // Je vérifie qu'il y a bien une et une seule balise <nav>
+        $this->assertSelectorExists('nav');
+        // Je vérifie que dans cette barre de nav, j'ai un seul lien
+        $this->assertCount(1, $crawler->filter('nav a'));
+        // Je vérifie que la balise <body> contient le mot "Lorem"
+        $this->assertSelectorTextContains('body', 'Lorem');
+    }
+}
